a70dc83041e0cce732b26756ad89871ed122ec2b
TomasMurillo26~ <to.murillo@duocuc.cl>
2022-05-23 19:16:04 -0400

    arreglado el problema que impedía arrancar el servidor

--- ProyectoWeb/Germinar/admin.py
+++ ProyectoWeb/Germinar/admin.py
@@ -1,7 +1,7 @@
 from atexit import register
 from django.contrib import admin
 
-from ProyectoWeb.Germinar.models import categoriaProducto
+from Germinar.models import categoriaProducto
 from .models import categoriaProducto,catSuscripcion,cliente
 # Register your models here.
 admin.site.register(categoriaProducto)
