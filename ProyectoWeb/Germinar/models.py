0de52f063aa8fb5d574d1a842c07edcb1ebf90aa
TomasMurillo26~ <to.murillo@duocuc.cl>
2022-05-25 10:34:34 -0400

    creando las clases de la base de datos

--- ProyectoWeb/Germinar/models.py
+++ ProyectoWeb/Germinar/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class categoriaProducto(models.Model):
+class catProducto(models.Model):
     idCategoria = models.IntegerField(primary_key=True, verbose_name= 'Id categoria')
     nombreCategoria = models.CharField(max_length=30, verbose_name= 'Nombre categoria')
 
@@ -18,6 +18,18 @@ class catSuscripcion(models.Model):
 class cliente(models.Model):
     idCliente = models.IntegerField(primary_key=True, verbose_name= 'Id cliente')
     nombreCliente = models.CharField(max_length=50, verbose_name= 'Nombre cliente' )
-    #categoria = models.foreignKey(Categoria, on_delete=models.CASCADE) Asi indicamos las llaves foraneas
+    contrasenna = models.CharField(max_length=50, verbose_name='Contrasenna')
+    correoElect = models.CharField(max_length=70, verbose_name='Correo Electronico')
+    fechaNac = models.DateField(verbose_name='Fecha de nacimiento')
+    suscripcion = models.ForeignKey(catSuscripcion, verbose_name= 'Tipo suscripcion')
+    ciudadCliente = models.CharField(max_length=50, verbose_name='Ciudad')
+    regionCliente = models.CharField(max_length=50, verbose_name='Region')
+    
     def __str__(self):
         return self.nombreCliente
+
+class producto(models.Model):
+    idProducto = models.IntegerField(primary_key=True, verbose_name= 'Id producto')
+    nombreProducto = models.CharField(max_length=150, verbose_name= 'Nombre del producto')
+
+
