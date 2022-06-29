9a17268ff9e2a72425cc96313b25617196b70a49
joxcastro <jox.castro@duocuc.cl>
2022-06-28 22:45:52 -0400

    asd

--- ProyectoWeb/Germinar/admin.py
+++ ProyectoWeb/Germinar/admin.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 from .models import catProducto,catSuscripcion,cliente,producto,compra,detalleCompra
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ["idProducto", "nombreProducto", "oferta"]
-    list_editable = ["oferta"]
-    list_filter = ["oferta"]
+    list_display = ["idProducto", "nombreProducto", "oferta","precio","categoria","descripcion"]
+    list_editable = ["oferta","precio","descripcion","categoria"]
+    list_filter = ["oferta","categoria"]
 # Register your models here.
 admin.site.register(catProducto)
 admin.site.register(catSuscripcion)
