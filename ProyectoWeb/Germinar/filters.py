50ef97043fb64c507bc7c13c54e95cb3dd8f2d0b
joxcastro <jox.castro@duocuc.cl>
2022-07-07 12:31:20 -0400

    css cambios filtro

--- ProyectoWeb/Germinar/filters.py
+++ ProyectoWeb/Germinar/filters.py
@@ -1,6 +1,6 @@
 from dataclasses import Field
 import django_filters
-from .models import producto
+from .models import producto, catProducto
 
 
 class FiltroVista(django_filters.FilterSet):
@@ -19,6 +19,13 @@ class FiltroVista(django_filters.FilterSet):
 			'precio':['lt','gt'],
         }
 
+
     def filter_by_order(self, queryset, name, value):
         expression = 'precio' if value == 'ascending' else '-precio'
         return queryset.order_by(expression)
+
+
+    def __init__(self, *args, **kwargs):
+        super(FiltroVista, self).__init__(*args, **kwargs)
+        categoria_defecto = catProducto.objects.get(idCategoria = 1)
+        self.form.initial['categoria'] = categoria_defecto
